a7036f9a80fee6c7aa40b225f239234a022d6483
Rahmat Syam <akyrisa03@gmail.com>
2017-09-09 12:44:01 +0800

    First

---
+++ app/src/main/java/com/example/android/searchlistview/Post.java
@@ -0,0 +1,33 @@
+package com.example.android.searchlistview;
+
+/**
+ * Created by user on 9/9/2017.
+ */
+
+public class Post {
+
+    private String postTitle;
+
+    private String postSubTitle;
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getPostSubTitle() {
+        return postSubTitle;
+    }
+
+    public void setPostSubTitle(String postSubTitle) {
+        this.postSubTitle = postSubTitle;
+    }
+
+    public Post(String postTitle, String postSubTitle) {
+        this.postTitle = postTitle;
+        this.postSubTitle = postSubTitle;
+    }
+}
